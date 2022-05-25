fb01a2c751ea24e3b2ad707200b840b98a8c158f
Brad Richardson <brichardson2468@gmail.com>
2022-05-25 12:30:21 -0400

    Put questions into exam

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 export default function App() {
 	const questions = [
 		{
-			questionText: 'What is the capital of France?',
+			questionText: 'What year is it?',
 			answerOptions: [
 				{ answerText: 'New York', isCorrect: false },
 				{ answerText: 'London', isCorrect: false },
@@ -12,7 +12,7 @@ export default function App() {
 			],
 		},
 		{
-			questionText: 'Who is CEO of Tesla?',
+			questionText: 'What season is it?',
 			answerOptions: [
 				{ answerText: 'Jeff Bezos', isCorrect: false },
 				{ answerText: 'Elon Musk', isCorrect: true },
@@ -21,7 +21,7 @@ export default function App() {
 			],
 		},
 		{
-			questionText: 'The iPhone was created by which company?',
+			questionText: 'What month is it?',
 			answerOptions: [
 				{ answerText: 'Apple', isCorrect: true },
 				{ answerText: 'Intel', isCorrect: false },
@@ -30,7 +30,133 @@ export default function App() {
 			],
 		},
 		{
-			questionText: 'How many Harry Potter books are there?',
+			questionText: 'What is the date today?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+		{
+			questionText: 'What time is it right now?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+		{
+			questionText: 'What country are we in?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+		{
+			questionText: 'What town/city are we in?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+		{
+			questionText: 'What district/county are we in?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+		{
+			questionText: 'Where are we now, and why?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+		{
+			questionText: 'Examiner names three objects (e.g. apple, table, penny) and asks patient to repeat. 1 point for each correct answer. AFTER have the patient commit those 3 words to memory, repeating until correct.',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+		{
+			questionText: 'Subtract 7 from 100, then repeat. Continue five times: 100, 93, 86, 79, 72, 65.',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+		{
+			questionText: 'Ask for the names of the three objects learned earlier.',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+		{
+			questionText: 'Name two objects (e.g. pen, watch).',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+		{
+			questionText: 'Repeat "No ifs, ands, or buts".',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+		{
+			questionText: 'Give three-stage command. 1 point per stage. (e.g. "Place index of right hand, on to your nose, then on your left ear")',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+		{
+			questionText: 'Ask the patient to read and obey a written command on a piece of paper. Instruction says "Close your eyes".',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+		{
+			questionText: 'Ask the patient to write a sentence. Must be sensible and have subject and verb.',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+		{
+			questionText: 'Ask patient to copy a pair of intersecting pentagons, with two overlaps.',
 			answerOptions: [
 				{ answerText: '1', isCorrect: false },
 				{ answerText: '4', isCorrect: false },
